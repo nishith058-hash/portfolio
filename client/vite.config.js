e350f49aea253db6e6c81cd4215af70612ab55ea
nishith <nishith672@gmail.com>
2025-12-31 21:40:27 +0530

    Update vite base for new repo

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: '/project-portfolio/',
+    base: '/portfolio/',
     server: {
         proxy: {
             '/api': {
